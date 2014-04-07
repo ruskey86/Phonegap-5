dad615a26f3845b868f4a59cc849412e0248a71e
ruskey86 <ruskey86@gmail.com>
2014-04-06 21:45:39 -0400

    change

--- js/script.js
+++ js/script.js
@@ -1,18 +1,43 @@
 // Put your custom code here
+window.onload = function(){
+	var flashlightstate = false;
+};
+
 function flashlightToggle()
 {
-	window.plugins.flashlight.toggle(flashlightsuccess,error("flashlight failed"),false);
+	if (flashlightstate)
+	{
+		window.plugins.flashlight.switchOn(flashlightOn, error("Flashlight failed to turn on"), false);
+		window.plugins.toast.show('Flashlight On', 'short', 'center');
+		flashlightstate = false;
+	}
+	else
+	{
+		window.plugins.flashlight.switchOff(flashlightOff, error("Flashlight failed to turn off"), false);
+		window.plugins.toast.show('Flashlight Off','short','center');
+		flashlightstate = true;
+	}
+
 }
-function flashlightsuccess()
+
+function flashlightOn()
 {
-	console.log("Flashlight toggled");
-	window.plugins.toast.show("Flashlight toggled",'short','center');
+	console.log("Flashlight on");
+	window.plugins.toast.show("Flashlight on");
 }
+
+function flashlightOff()
+{
+	console.log("Flashlight off");
+	window.plugins.toast.show("Flashlight off");
+}
+
 function error(message)
 {
 	console.log(message);
 	window.plugins.toast.show(message, 'long', 'center');
 }
+
 document.addEventListener("backbutton", function()
 {
 	// pass exitApp as callbacks to the switchOff method
